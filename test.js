b916059906c15c4cb58623f4f8b601913d40329a
anna <malaev228@gmail.com>
2020-04-29 19:30:27 +0300

    comment

--- test.js
+++ test.js
@@ -2,3 +2,8 @@ const chalk = require('chalk')
 
 console.log('hi');
 console.log(chalk.blue('Hello world!'));
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.blue('Hello world!'));
